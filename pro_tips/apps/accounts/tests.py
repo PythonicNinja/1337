56325db8bc4026d0e8aa700830d15b3f5eb87822
Wojtek Nowak <vojtek.nowak@gmail.com>
2014-05-16 01:14:00 +0200

    1.travis.yml 2.tests

--- pro_tips/apps/accounts/tests.py
+++ pro_tips/apps/accounts/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from django.core.urlresolvers import resolve
+from django.core.urlresolvers import resolve, reverse
 
 from django.contrib.auth import views as django_views
 
@@ -14,6 +14,16 @@ from pro_tips.apps.accounts import views
 
 
 class AccountsTests(TestCase):
+
+    # Status 200 test views
+    # TODO: add more test like 200
+    def test_200_index(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_200_tips(self):
+        response = self.client.get(reverse('tips'))
+        self.assertEqual(response.status_code, 200)
 
     # Testing url mappings
     def test_login_url_resolves_to_login_view(self):
---
+++ pro_tips/settings/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'wojtek'
+
+from base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db',
+    }
+}
